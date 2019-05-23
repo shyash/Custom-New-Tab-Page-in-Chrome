8b4da75d4563027599212647190ad90ab4e7e46f
shyash <shyash222@gmail.com>
2019-05-23 16:28:24 +0530

    update newtab.js

--- newtab.js
+++ newtab.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  (function() {
+  
      chrome.storage.local.get(['content'], function(result) {
           if(result.content){
             document.querySelector("html").innerHTML = result.content
           }
           
         });
-  })()
-  
+ 
 });
 	   
   
